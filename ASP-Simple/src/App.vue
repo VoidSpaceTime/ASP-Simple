e8fa7d0c537f1f5eda54a6f84f91e4c267219abb
VoidSpaceTime <Space_Time_@outlook.com>
2025-02-13 02:18:18 +0800

    update vue

--- ASP-Simple/src/App.vue
+++ ASP-Simple/src/App.vue
@@ -1,44 +1,12 @@
 <template>
-  <div class="login-container">
-    <el-h1> 登录 </el-h1>
-    <<el-form :model="form" label-width="80px" :inline="false" size="normal">
-
-      <div class="form-group">
-        <el-label for="username">用户名</el-label>
-        <el-input v-model="form.username" type="text" id="username" required />
-
-        <el-label for="password">密码</el-label>
-        <el-input v-model="form.password" type="password" id="password" required />
-      </div>
-      <el-button type="submit">登录</el-button>
-      <el-button>注册</el-button>
-
-      </el-form>
-
-  </div>
+  <Login />
 </template>
 
 <script setup lang="ts">
 import { reactive, ref } from 'vue';
+import Login from './components/Login.vue';
 
 
-const form = reactive({
-  username: '',
-  password: ''
-});
-
-const errorMessage = ref('');
-
-const handleSubmit = () => {
-  if (form.username === 'admin' && form.password === 'password') {
-    alert('登录成功');
-    errorMessage.value = '';
-  } else {
-    errorMessage.value = '用户名或密码错误';
-  }
-};
-
-;
 </script>
 
 <style scoped>
--- ASP-Simple/src/components/Login.vue
+++ ASP-Simple/src/components/Login.vue
@@ -1,23 +1,20 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form>
-            <el-form>
-                <el-label>账号</el-label>
-                <el-input class="User" v-model="user"> </el-input>
-                <el-label>密码</el-label>
-                <el-input class="Password" show-password v-model="psw"> </el-input>
-                <el-button type="primary" @Click="login">登录</el-button>
-                <el-button>注册</el-button>
-            </el-form>
-        </form>
-    </div>
+    <el-div class="login-container">
+        <el-h1>Login</el-h1>
+        <el-form>
+            <el-label>账号</el-label>
+            <el-input class="User" v-model="user"> </el-input>
+            <el-label>密码</el-label>
+            <el-input class="Password" show-password v-model="psw"> </el-input>
+            <el-button type="primary" @Click="login">登录</el-button>
+            <el-button>注册</el-button>
+        </el-form>
+    </el-div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
 import { ref } from 'vue';
-// import  {Axios}  from 'axios';
 let user = ref('');
 let psw = ref('');
 
@@ -33,4 +30,20 @@ function login() {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-container {
+    position:relative;
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 2rem;
+    border: 1px solid #ccc;
+    border-radius: 16px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+}
+el-h1 {
+    text-align: center;
+    font-size: 24px;
+    margin-bottom: 200px;
+}
+</style>
