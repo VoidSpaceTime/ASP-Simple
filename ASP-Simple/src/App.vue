6efd7d60af383e1fe0027bd8d4ae234de645f1fe
VoidSpaceTime <Space_Time_@outlook.com>
2025-02-12 23:12:53 +0800

    update

--- ASP-Simple/src/App.vue
+++ ASP-Simple/src/App.vue
@@ -1,47 +1,62 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="login-container">
+    <el-h1> 登录 </el-h1>
+    <<el-form :model="form" label-width="80px" :inline="false" size="normal">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="form-group">
+        <el-label for="username">用户名</el-label>
+        <el-input v-model="form.username" type="text" id="username" required />
 
-  <main>
-    <TheWelcome />
-  </main>
+        <el-label for="password">密码</el-label>
+        <el-input v-model="form.password" type="password" id="password" required />
+      </div>
+      <el-button type="submit">登录</el-button>
+      <el-button>注册</el-button>
+
+      </el-form>
+
+  </div>
 </template>
 
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+
+const form = reactive({
+  username: '',
+  password: ''
+});
+
+const errorMessage = ref('');
+
+const handleSubmit = () => {
+  if (form.username === 'admin' && form.password === 'password') {
+    alert('登录成功');
+    errorMessage.value = '';
+  } else {
+    errorMessage.value = '用户名或密码错误';
+  }
+};
+
+;
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.login-container {
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 2rem;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  text-align: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.form-group {
+  margin-bottom: 1rem;
 }
 </style>
---
+++ ASP-Simple/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form>
+            <el-form>
+                <el-label>账号</el-label>
+                <el-input class="User" v-model="user"> </el-input>
+                <el-label>密码</el-label>
+                <el-input class="Password" show-password v-model="psw"> </el-input>
+                <el-button type="primary" @Click="login">登录</el-button>
+                <el-button>注册</el-button>
+            </el-form>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+// import  {Axios}  from 'axios';
+let user = ref('');
+let psw = ref('');
+
+function login() {
+    axios.post('http://localhost:3000/login', {
+        user: user.value,
+        psw: psw.value
+    }).then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+</script>
+
+<style scoped></style>
